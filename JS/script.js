047297d73f432474b085d50d15f41b0d43c4ef1f
KVN1329 <kvn1329@Gmail.com>
2022-01-20 19:06:19 +0100

    therd commit

--- JS/script.js
+++ JS/script.js
@@ -19,22 +19,58 @@
     }
 Проверить, чтобы все работало без ошибок в консоли */
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    generes: {},
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     generes: {},
+//     privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// if (4 == 4) {
+//     console.log('Ok!');
+// } else {
+//     console.log('error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('mnogo');
+// } else {
+//     console.log('ok');
+// }
+
+// (num === 50) ? console.log('ok') : console.log('Error');
+
+// Условия
+
+// const num = '50';
+// switch (num) {
+//     case 49:
+//         console.log('net');
+//         break;
+//     case 100:
+//         console.log('net');
+//         break;
+//     case '50':
+//         console.log('YES');
+//         break;
+//     default:
+//         console.log('another day');
+//         break;
+// }
